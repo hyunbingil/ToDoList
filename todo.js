fd2ac203449f739be26edfb36b0d5bc525be1c31
hyunbingil <super199g@naver.com>
2020-02-15 02:28:40 +0900

    toDoList 삭제까지완료

--- todo.js
+++ todo.js
@@ -4,10 +4,27 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];//해야할 일을 생성했을 때 이 array에 추가되도록 한다.
+let toDos = [];//해야할 일을 생성했을 때 이 array에 추가되도록 한다.(비어있다는 것을 기억하렴)
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);//3번째줄 친구 데려온거
+    const cleanToDos = toDos.filter(function(toDo){
+            return toDo.id !== parseInt(li.id);
+            //parseInt : 숫자형으로 바꿔!
+        
+        }//forEach에서 function을 실행하는 것 같이 각각의 item과 같이 실행될겨!(이거랑 forEach 중요!)
+    );
+    //filter는 array의 모든 아이템을 통해 함수를 실행하고 true인 아이템들만 가지고 새로운 array 만든다.
+    toDos = cleanToDos;
+    saveToDos();
+}//어떤 버튼이 클릭되는지 알려면 부모를 먼저 알아야하고 우리가 id를 줘놨잖아 그걸로 알 수 있어!
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+    //localStorage에는 string으로 밖에 저장이 되지 않아요 그래서 string으로 바꿔주는겁니다.^.^
+    //json이 뭔데! JavaScript Object Notation의 줄인말.. 데이터 전달 시 자바스크립트가 그걸 다룰 수 있도록 object로 바꿔주는 기능이래..
 }
 
 function paintToDo(text){
@@ -16,6 +33,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -41,7 +59,11 @@ function handleSubmit(event){
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS); 
     if(loadedToDos !== null ){
-
+        const parsedToDos = JSON.parse(loadedToDos);//object로 바꿔줍니당~!
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);//각각에 대해서 paintToDo라는 function 실행
+        })
+        //기본적으로 함수 실행, array에 담겨있는 것들을 각각에 한번씩 함수를 실행시켜주는거!
     }
 
 }   
