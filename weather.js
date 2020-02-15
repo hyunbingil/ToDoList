bec06cc17c7b4248408a745aca4f31a5e2bad85e
hyunbingil <super199g@naver.com>
2020-02-15 22:06:23 +0900

    ToDoList 최종(꾸미기X.ver)

--- weather.js
+++ weather.js
@@ -1,6 +1,25 @@
-const WEATHER_API_KEY = "5035cdeee349093915fac82b42b84d6a";
+const weather = document.querySelector(".js-weather");
 
+const WEATHER_API_KEY = "5035cdeee349093915fac82b42b84d6a";
 const COORDS = "coords";
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}Cº in ${place}`;
+    });
+    //then 사용 이유 : fetch가 완료되길 기다려야 하기 때문. 안하면 정상적으로 완료X일수도..
+
+}
+
+/*javascript는 웹사이트로 request를 보내고 응답을 통해 데이터 얻기 가능.
+~> 가져온 데이터를 refresh없이도 적용가능해서!*/
 
 
 function saveCoords(coordsObj){
@@ -14,6 +33,7 @@ function handleGeoSuccess(position){
         longitude: longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -28,7 +48,8 @@ function loadCoords(){
         askForCoords();
     }
     else{
-        getWeather();
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
     }
 }
 
